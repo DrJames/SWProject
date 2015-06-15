eac7632f5d8dd5ef7b8284943027c72f36e99f80
DF.thangld <DF.thangld@hotmail.com>
2015-06-15 11:57:53 +0300

    random device every 5 seconds

--- app/src/main/java/com/swproject/fi/swproject/MainActivity.java
+++ app/src/main/java/com/swproject/fi/swproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.swproject.fi.swproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +12,15 @@ import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
     private ImageViewAdapter adapter;
     private List<Device> deviceList;
+    private int deviceIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         GridView gridView = (GridView) findViewById(R.id.gridView);
-        deviceList = new ArrayList<>();
+        //deviceList = Collections.synchronizedList(new ArrayList<Device>());
+        deviceList = new ArrayList<Device>();
         adapter = new ImageViewAdapter(getApplicationContext(), R.layout.grid_item, deviceList);
 
         gridView.setAdapter(adapter);
@@ -36,6 +41,95 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         btnAdd.setOnClickListener(this);
         btnRemove.setOnClickListener(this);
+
+        //this.runOnUiThread();
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (true)
+                {
+                    // create random variable
+                    Random r = new Random();
+
+                    // decide the action is add new device or delete old one
+                    // random = 0 => delete a random old device
+                    // random = 1 => create new device
+                    // if there are less than 10 devices then chances of adding new device are higher than removing
+                    int action = 0;//
+                    if (deviceList.size() <= 3)
+                        action = 1;
+                    else if (deviceList.size() <= 10)
+                    {
+                        action = r.nextInt(10);
+                        if (action <= 7)
+                            action = 1;
+                        else
+                            action = 0;
+                    }
+                    else
+                        action = r.nextInt(1);
+
+                    if (action == 1) // create new device
+                    {
+                        int deviceSign = 0;
+                        String device_name = "";
+                        switch(r.nextInt(4))
+                        {
+                            case 0:
+                                deviceSign = R.drawable.desktop;
+                                device_name = "Desktop ";
+                                break;
+                            case 1:
+                                deviceSign = R.drawable.laptop;
+                                device_name = "Laptop ";
+                                break;
+                            case 2:
+                                deviceSign = R.drawable.phone;
+                                device_name = "Phone ";
+                                break;
+                            case 3:
+                                deviceSign = R.drawable.printer;
+                                device_name = "Printer ";
+                                break;
+                            default:
+                                deviceSign = R.drawable.desktop;
+                                device_name = "Desktop ";
+                                break;
+                        }
+                        //int index = deviceList.size() + 1;
+                        deviceIndex++;
+                        deviceList.add(new Device(deviceSign, device_name + deviceIndex,
+                                "192.168.0." + deviceIndex, "C8:F7:33:06:64:2C"));
+                    }
+                    else // delete a random old device
+                    {
+                        if (deviceList.size() != 0)
+                        {
+                            int randomIndex = r.nextInt(deviceList.size() - 1);
+                            deviceList.remove(randomIndex);
+                        }
+                    }
+
+
+                    // notify change
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+
+                    // wait 5 seconds
+                    try {
+                        Thread.sleep(5000);
+                    }
+                    catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }).start();
+
     }
 
     @Override
@@ -62,9 +156,11 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     private void addItem(){
         int index = deviceList.size() + 1;
-        deviceList.add(new Device(R.drawable.desktop, "Device " + index,
-                "192.168.0." + index, "C8:F7:33:06:64:2C"));
+		deviceIndex++;
+        deviceList.add(new Device(R.drawable.desktop, "Device " + deviceIndex,
+                "192.168.0." + deviceIndex, "C8:F7:33:06:64:2C"));
         adapter.notifyDataSetChanged();
+
     }
 
     private void removeItem(){
