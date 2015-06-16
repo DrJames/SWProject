c898b4d2fef3f7f9e68de42f37b9c73c0ebf53c5
DF.thangld <DF.thangld@hotmail.com>
2015-06-16 14:21:51 +0300

    Device now can be created by JSON string

--- app/src/main/java/com/swproject/fi/swproject/Device.java
+++ app/src/main/java/com/swproject/fi/swproject/Device.java
@@ -1,5 +1,8 @@
 package com.swproject.fi.swproject;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by alex on 7.6.2015.
  */
@@ -14,6 +17,45 @@ public class Device {
         this.name = name;
         this.ipAddress = ipAddress;
         this.macAddress = macAddress;
+    }
+
+    /* added by Thangld on 16.6.2015
+    json string should have the following format:
+    {
+        'name' : 'name of the device',
+        'ipAdress' : 'ip of the device',
+        'macAddress' : 'mac address of the device',
+        'type' : 'type of the device (currently only work with laptop, phone, desktop and printer)'
+    }
+     */
+    public Device(String jsonString)
+    {
+        try {
+            JSONObject jsonObj = new JSONObject(jsonString);
+            this.name = jsonObj.getString("name");
+            this.ipAddress = jsonObj.getString("ipAddress");
+            this.macAddress = jsonObj.getString("macAddress");
+            switch(jsonObj.getString("type"))
+            {
+                case "desktop":
+                    this.icon = R.drawable.desktop;
+                    break;
+                case "laptop":
+                    this.icon = R.drawable.laptop;
+                    break;
+                case "phone":
+                    this.icon = R.drawable.phone;
+                    break;
+                case "printer":
+                    this.icon = R.drawable.printer;
+                    break;
+                default:
+                    this.icon = R.drawable.desktop;
+                    break;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     public Integer getIcon(){
