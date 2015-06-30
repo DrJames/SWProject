8b79f0b1ecf26f8aafa010a6398ac48f9963b03e
ZJJ1991 <zhoujunjie.10@gmail.com>
2015-06-30 13:36:04 +0300

    Prototype

--- app/src/main/java/com/swproject/fi/swproject/SomeActivity.java
+++ app/src/main/java/com/swproject/fi/swproject/SomeActivity.java
@@ -1,5 +1,6 @@
 package com.swproject.fi.swproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,11 +15,11 @@ public class SomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_);
-        Button btnOk = (Button) findViewById(R.id.btnOk);
-        btnOk.setOnClickListener(new View.OnClickListener() {
+        Button btn_report = (Button) findViewById(R.id.btn_Report);
+        btn_report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                setContentView(R.layout.activity_report);
             }
         });
     }
